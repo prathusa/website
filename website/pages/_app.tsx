f7a9391ee35c2fef361b812e9c428e55330fa8d4
Pratham <prathusaf@gmail.com>
2023-01-12 19:51:38 -0600

    corrected google analytics implementation

--- website/pages/_app.tsx
+++ website/pages/_app.tsx
@@ -1,21 +1,40 @@
 import '../styles/globals.css'
 import Script from 'next/script'
 import type { AppProps } from 'next/app'
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import * as gtag from '../lib/gtag';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: any) => {
+      gtag.pageview(url);
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
-      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXX" />
+      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-1WNXG5Q35W"></Script>
       <Script
         id='google-analytics'
         strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
+        dangerouslySetInnerHTML = {{
           __html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-1WNXG5Q35W', { page_path: window.location.pathname, });
-          `,
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-1WNXG5Q35W', {
+              page_path: window.location.pathname,
+            });
+            `,
         }}
       />
       <Component {...pageProps} />
