e1898eeb77239f6ea7ada479b5d563d0e793a80d
Pratham <prathusaf@gmail.com>
2023-01-15 04:52:40 -0600

    Updated MD5 Page

--- website/pages/projects/md5_hash.tsx
+++ website/pages/projects/md5_hash.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
-import img from '/public/assets/projects/md5_hash/MD5 Brute Force Timings.png';
-// import img from 'public/assets/me.png'
+import bg from '/public/assets/projects/md5_hash/md5_bg.png';
+import img from '/public/assets/projects/md5_hash/MD5_algorithm.png';
 import { RiRadioButtonFill } from 'react-icons/ri';
 import Link from 'next/link';
 
@@ -12,13 +12,12 @@ const md5_hash = () => {
       <div className='w-screen h-[50vh] relative'>
         <div className='absolute top-0 left-0 w-full h-[50vh] bg-black/70 z-10' />
         <Image
-          className='absolute z-1'
+          className='object-cover absolute z-1'
           fill
-          object-cover
-          src={img}
+          src={bg}
           alt='/'
         />
-        <div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2'>
+        <div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] z-10 p-2'>
           <h2 className='py-2'>MD5 Hash Cracker</h2>
           <h3>CUDA | C++</h3>
         </div>
@@ -31,8 +30,22 @@ const md5_hash = () => {
             <p className='underline cursor-pointer'>Back</p>
           </Link>
           <h2>Overview</h2>
+          {/* <div className="mx-10"> */}
+          <Image
+            className='float-right p-8'
+            src={img}
+            width={300}
+            alt='MD5 Bitwise Algorithm Diagram'
+          />
+          {/* </div> */}
           <p className='py-8'>
-            The MD5 hash function is commonly used to verify the integrity of data, such as the integrity of downloaded files or messages transmitted over a network. However, the MD5 encryption scheme has been found to be vulnerable to a number of attacks and is no longer considered secure. This project aims to highlight the vulnerabilities of the MD5 encryption scheme by using a GPU (Graphics Processing Unit) to brute force it. The project involves creating a program that takes an MD5 hash value as input and uses a GPU to quickly try every possible combination of characters until it finds a match. The program could then be used to demonstrate the weakness of the MD5 encryption scheme by showing how quickly it can be broken using a brute-force attack. Overall, this project highlights the significance of using stronger and more secure encryption methods to protect sensitive data.
+            The MD5 hash function is commonly used to verify the integrity of data, such as the integrity of downloaded files or messages
+            transmitted over a network. However, the MD5 encryption scheme has been found to be vulnerable to a number of attacks and is
+            no longer considered secure. This project aims to highlight the vulnerabilities of the MD5 encryption scheme by using a GPU
+            (Graphics Processing Unit) to brute force it. The project involves creating a program that takes an MD5 hash value as input
+            and uses a GPU to quickly try every possible combination of characters until it finds a match. The program could then be used
+            to demonstrate the weakness of the MD5 encryption scheme by showing how quickly it can be broken using a brute-force attack.
+            Overall, this project highlights the significance of using stronger and more secure encryption methods to protect sensitive data.
           </p>
           <a
             href='https://github.com/prathusa/MD5_Hash_Cracker'
